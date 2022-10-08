aaf50e912f16e21fbf0cc3b0625c85faf3598454
laurium <laurium@bugger.local>
2022-10-08 22:46:46 +0000

    c004 | add comments section to post detail page

--- blog/admin.py
+++ blog/admin.py
@@ -15,6 +15,10 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'content')
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -37,7 +37,8 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name="comments", on_delete=models.CASCADE)
     author = models.ForeignKey(user_model, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
