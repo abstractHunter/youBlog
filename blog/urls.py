e35d14f03f388dffb43df0fee03e8b51e8fd9923
laurium <laurium@bugger.local>
2022-10-09 18:44:02 +0000

    c011 | add blog creation page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostCreate
 from django.urls import path
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
+    path('new/', PostCreate.as_view(), name='post_new'),
     path('<str:username>/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
@@ -12,3 +14,14 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'slug', 'content', 'status', 'tags']
+    success_url = reverse_lazy('my_profile')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostCreate, self).form_valid(form)
