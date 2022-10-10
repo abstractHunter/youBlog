82bed15cc5161d6a1ce3d3b38a0525e9d51316bd
laurium <laurium@bugger.local>
2022-10-10 08:31:12 +0000

    c013 | add 'post edit' and 'delete confirmation' views and templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
-from .views import PostList, PostDetail, PostCreate
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.urls import path
 
 urlpatterns = [
-    path('', PostList.as_view(), name='home'),
-    path('new/', PostCreate.as_view(), name='post_new'),
-    path('<str:username>/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('', PostListView.as_view(), name='home'),
+    path('new/', PostCreateView.as_view(), name='post_new'),
+    path('<str:username>/<slug:slug>/',
+         PostDetailView.as_view(), name='post_detail'),
+    path('<str:username>/<slug:slug>/edit',
+         PostUpdateView.as_view(), name='post_edit'),
+    path('<str:username>/<slug:slug>/delete',
+         PostDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,24 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .models import Post
 
 # Create your views here.
 
 
-class PostList(ListView):
+class PostListView(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_at')
     template_name = 'home.html'
 
 
-class PostDetail(DetailView):
+class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
 
-class PostCreate(LoginRequiredMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'blog/post_form.html'
     fields = ['title', 'slug', 'content', 'status', 'tags']
@@ -25,3 +27,26 @@ class PostCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(PostCreate, self).form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'slug', 'content', 'status', 'tags']
+    success_url = reverse_lazy('my_profile')
+
+    def get(self, request, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            return redirect('my_profile')
+        return super().get(request, *args, **kwargs)
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('my_profile')
+
+    def get(self, request, *args, **kwargs):
+        if self.get_object().author != self.request.user:
+            return redirect('my_profile')
+        return super().get(request, *args, **kwargs)
