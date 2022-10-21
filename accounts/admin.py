16135bff205eb4fc15eb7e1e747735a7322e1e18
laurium <laurium@bugger.local>
2022-10-21 09:22:56 +0000

    c026 | update User model | add is_blogger attribute to show if a user is a blogger or not

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,7 +11,19 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ["email", "username", ]
+    list_display = ["email", "username", "is_blogger"]
+
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            'Custom Fields',
+            {
+                'fields': (
+                    'is_blogger',
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     # a user is a blogger if they have verified their email address
     is_blogger = models.BooleanField(default=False)
-    pass
 
     def __str__(self):
         return self.username
